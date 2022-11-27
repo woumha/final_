40db7a4d6c3dd79e0c35fbddff39b94ee1dc77e7
axp987 <86609910+axp987@users.noreply.github.com>
2022-11-28 05:41:28 +0900

    11-28 Rean Me 읽어주세요!

--- Five_ONE_FINAL/src/main/java/com/youtube/model/YouTubeDAO.java
+++ Five_ONE_FINAL/src/main/java/com/youtube/model/YouTubeDAO.java
@@ -1,5 +1,27 @@
 package com.youtube.model;
 
 public interface YouTubeDAO {
-
+	// DongWhan Method
+	
+	// DongWhan End
+	
+	
+	// Eunji Method
+	
+	// Eunji End
+	
+	
+	// Hohan Method
+	
+	// Hohan End
+	
+	
+	// Jaey Method
+	
+	// Jaey End
+	
+	
+	// Seungjun Method
+	
+	// Seungjun End
 }
