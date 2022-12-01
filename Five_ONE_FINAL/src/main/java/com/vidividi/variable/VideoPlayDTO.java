aacce2f999667f6184193304a43788c1e14b99b1
user1 <user1@DESKTOP-50SGODH>
2022-12-01 17:50:29 +0900

    12/1

---
+++ Five_ONE_FINAL/src/main/java/com/vidividi/variable/VideoPlayDTO.java
@@ -0,0 +1,21 @@
+package com.vidividi.variable;
+
+import lombok.Data;
+
+@Data
+public class VideoPlayDTO {
+
+	private int video_code;
+	private int channel_code;
+	private String video_title;
+	private String video_cont;
+	private String video_img;
+	private int video_good;
+	private int video_bad;
+	private int video_view_cnt;
+	private String video_hash;
+	private String video_regdate;
+	private int video_open;
+	private int category_code;
+	
+}
