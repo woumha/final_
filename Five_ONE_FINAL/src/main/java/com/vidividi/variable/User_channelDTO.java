30830c5a11403db05c414bc10aadd54c341d5977
user1 <user1@DESKTOP-50SGODH>
2022-12-06 09:45:39 +0900

    장동환_12-05 마무리

---
+++ Five_ONE_FINAL/src/main/java/com/vidividi/variable/User_channelDTO.java
@@ -0,0 +1,18 @@
+package com.vidividi.variable;
+
+import lombok.Data;
+
+@Data
+public class User_channelDTO {
+
+	private int channel_code;
+	private String channel_name;
+	private String channel_banner;
+	private String channel_cont;
+	private int channel_like;
+	private String channel_live;
+	private String channel_date;
+	private String channel_lastupload;
+	private int member_code;
+	
+}
