6b0a55cb1446bd91e95b24f000a441e9d09eb26a
j-heo <6541hz@gmail.com>
2022-12-08 17:50:12 +0900

    1208 저녁커밋 join.jsp 꾸미는 중

--- Five_ONE_FINAL/src/main/java/com/vidividi/five/one/HomeController.java
+++ Five_ONE_FINAL/src/main/java/com/vidividi/five/one/HomeController.java
@@ -20,8 +20,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.vidividi.model.ChannelDAO;
 import com.vidividi.model.MemberDAO;
 import com.vidividi.model.VidividiDAO;
+import com.vidividi.variable.ChannelDTO;
 import com.vidividi.variable.MemberDTO;
 import com.vidividi.variable.VideoPlayDTO;
 
@@ -44,10 +46,16 @@ public class HomeController {
         model.addAttribute("video_list", list);
         
         if (session.getAttribute("MemberCode")!=null) {
+        	
         	String memberCode = (String)session.getAttribute("MemberCode");
         	String lastChannelCode = (String)session.getAttribute("LastChannelCode");
         	System.out.println(memberCode);
 			MemberDTO memberDTO = memberDAO.getMember(memberCode);
+			
+			/*
+			 * List<ChannelDTO> AllChannelList = ChannelDAO.getAllChannelList(memberCode);
+			 */
+			
             model.addAttribute("MemberCode", memberCode);
 			model.addAttribute("MemberName", memberDTO.getMember_name());
 			model.addAttribute("LastChannelCode", lastChannelCode);
