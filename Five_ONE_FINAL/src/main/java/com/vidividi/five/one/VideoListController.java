e9736e80743bf489b69ddc6a004af1393a5d7a5e
user1 <user1@DESKTOP-50SGODH>
2022-12-20 17:52:29 +0900

    1220 -사이드 배너 + 커서 이미지 - 소트 정렬(고치는 중...)

--- Five_ONE_FINAL/src/main/java/com/vidividi/five/one/VideoListController.java
+++ Five_ONE_FINAL/src/main/java/com/vidividi/five/one/VideoListController.java
@@ -3,11 +3,9 @@ package com.vidividi.five.one;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.vidividi.model.VidividiDAO;
@@ -19,30 +17,49 @@ public class VideoListController {
 	@Inject
     private VidividiDAO dao;
 	
+	
 	@ResponseBody
 	@RequestMapping("mainVideoList.do")
 	public List<VideoPlayDTO> mainList(int page){
 		
-	 	int rowsize = 12;
+	 	int rowsize = 20;
 		int startNo = (page * rowsize) - (rowsize - 1);
 		int endNo = (page * rowsize);
 		
 		List<VideoPlayDTO> list = this.dao.mainVideo(startNo, endNo);
-
+				
+		System.out.println("startno>>>" + startNo);
+		System.out.println("endNo>>>" + endNo);
+		System.out.println(list);
 		return list;
 	}
 	
+	//최신순 정렬
 	@ResponseBody
 	@RequestMapping("mainVideoList_up.do")
 	public List<VideoPlayDTO> mainList_up(int page){
 		
-		int rowsize = 12;
+		int rowsize = 20;
 		int startNo = (page * rowsize) - (rowsize - 1);
 		int endNo = (page * rowsize);
 		
 		List<VideoPlayDTO> list2 = this.dao.mainVideo_up(startNo, endNo);
 		
 		return list2;
+	}
+	
+	//소트 정렬
+	@ResponseBody
+	@RequestMapping("mainVideoList_sort_cook.do")
+	public List<VideoPlayDTO> mainList_sort(int page){
+		
+		int rowsize = 20;
+		int startNo = (page * rowsize) - (rowsize - 1);
+		int endNo = (page * rowsize);
+		
+		List<VideoPlayDTO> list3 = this.dao.mainVideo_sort_music(startNo, endNo);
+		
+		return list3;
 	}
 
 
